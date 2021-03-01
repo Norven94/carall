87e0dff8bf79b60b9f34b33bcdfd24cb62a20025
celiltat2361 <celilbtc@gmail.com>
2021-03-01 14:39:40 +0100

    added new css codes in Car.js and created new css file i css folder

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,6 +1,5 @@
 import { useHistory } from "react-router-dom";
-import React, { useContext, useState } from "react";
-import { CarContext } from "../contexts/CarContext";
+import { car, boxDetails } from '../css/Car.module.css'
 
 export default function Car (props) {
   const history = useHistory();
@@ -10,15 +9,14 @@ export default function Car (props) {
 
 
   return (
-    <div className="car">
+    <div className={car}>
       <img onClick={goToProduct} src={props.car.image} alt={"Image of " + props.car.make + " " + props.car.model + " " + props.car.year} />
       
-          <div className="box-details">
+          <div className={boxDetails}>
             
             <h2>{props.car.make + " " + props.car.model}</h2>
-            <h3>{props.car.price} kr</h3>
-            <p>{props.car.descShort}</p>
-            <p>{props.car.descLong}</p>
+            <h3>{props.car.year} {props.car.city}</h3>
+            <h4>{props.car.price} kr</h4>
             <button>Add to Cart</button>
           </div>
         </div>
