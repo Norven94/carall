62cac0549f7834d741c0805d7ef586dc73f3ede9
Jakob <jsandberg_8@hotmail.com>
2021-03-01 21:38:44 +0100

    Added bootstrap to CartProduct

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -1,5 +1,6 @@
 import {useContext} from "react"
 import {CarContext }from "../contexts/CarContext"
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function CartProduct(props) {
     /*
@@ -9,6 +10,7 @@ export default function CartProduct(props) {
 
     return (
         <div className="cart-product">
+
             <button onClick={() => removeProduct(props)}>Remove</button>
         </div>
     )
