175d0bd34d0c2e189485a0ca1df200df83dfa978
Kyd <info@kydkit.com>
2021-03-01 10:28:35 +0100

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { BrowserRouter, Route } from "react-router-dom";
 
+import ContextProvider from './contexts/CarContext'
+import Navbar from './components/Navbar'
+import Home from './'
+
 function App() {
   return (
-  <BrowserRouter>
     <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        {/* ContextProvider must enclose all components that will have access to its data */}
+        <ContextProvider>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </ContextProvider>
 
+      </BrowserRouter>
     </div>
-  </BrowserRouter>
   )
 }
 
