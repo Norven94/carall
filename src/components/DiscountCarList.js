8c7598dce621fb950e1bcfd21def205cbe6f0b71
Kyd <info@kydkit.com>
2021-03-01 15:27:32 +0100

    change car directory

--- src/components/DiscountCarList.js
+++ src/components/DiscountCarList.js
@@ -1,13 +1,12 @@
 import { useContext } from "react"
 import CarContext from '../contexts/CarContext'
-
-import Car from '../components/Car'
+import Car from './Car'
 
 const DiscountCarList = () => {
   const {carDiscount} = useContext(CarContext)
 
   return (
-    <div>
+    <div classname="cardiscount-container">
       {carDiscount.map((car) =>(
         <Car key={car.vin} car={car} />
       ))}
