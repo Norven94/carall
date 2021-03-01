c9f5ea2465341df928e0d826e7bcf0b5eeff785f
Oskar Anderberg <otto.anderberg@gmail.com>
2021-03-01 11:23:51 +0100

    Added props on image in Car.js
    Added props on image in Car.js to be able to test redirect

--- src/components/Car.js
+++ src/components/Car.js
@@ -4,13 +4,13 @@ export default function Car (props) {
   const history = useHistory();
 
   const goToProduct = () => {
-    history.push("/product/" + props.vin)
+    history.push("/product/" + props.post.vin)
   }
 
   return (
     <div>
       Car.js component
-      <img onClick={goToProduct} img="" alt="" />
+      <img onClick={goToProduct} img={props.post.image} alt={"Image of " + props.car.make + " " + props.car.model + " " + props.car.year} />
     </div>
   )
 }
