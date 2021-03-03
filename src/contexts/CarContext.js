cfd648c2db93e48e895a409dddad798e20ccf101
Mikaela <M.Norrelokke@hotmail.com>
2021-03-03 12:14:44 +0100

    Total price for product function and some changes in nav

--- src/contexts/CarContext.js
+++ src/contexts/CarContext.js
@@ -1,9 +1,10 @@
-import {createContext, useState} from "react"
+import {createContext, useState, useEffect} from "react"
 import data from "../assets/json/cars.json"
 export const CarContext = createContext();
 
 const CarContextProvider =(props)=>{
-
+  const [totalProducts, setTotalProducts] = useState(0)
+  const [totalOrder, setTotalOrder] = useState(0)
  
   const [cars] = useState(()=>data.map((car) => {          
     return{
@@ -44,22 +45,23 @@ const addToCart = (product) => {
   setCart([...cart, product])
 }
 
-const total = () =>{
-  let totalVal = 0;
-  for(let i = 0; i < cart.length; i++ ){
-      totalVal += cart[i].price
-  }
-  setTotalOrder(totalVal)
-}
-
-
-  /*
-  Add when cart array is added to this file
-
   const removeProduct = (product) => {
     setCart(cart.filter((p) => p !== product));
   }
 
+  useEffect(
+    () =>{  
+        setTotalProducts(cart.length)
+        setTotalOrder(cart.reduce((acc,num) => {
+          console.log(acc)
+          return acc+num
+          
+        },0 ))
+    }, [cart]   
+)
+// acc = totala värdet i cart (börjar alltid på 0)
+// num = objektet (bilen) 
+// returnerar det nya priset
 
   const [carDiscount, setCarDicount] = useState(() => cars.filter((car) => car.isDiscount === true))
 
@@ -71,6 +73,8 @@ const total = () =>{
     cart,
     addToCart,
     removeProduct,
+    totalProducts,
+    totalOrder,
   }
 
   return(
