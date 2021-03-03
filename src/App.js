90b393f527511745ee863e48b258c73ee49df35a
Mikaela <M.Norrelokke@hotmail.com>
2021-03-03 10:18:11 +0100

    import './App.scss'; into index.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 //import About from './pages/About'
 import CartPage from './pages/CartPage'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
