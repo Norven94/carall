f96a909e698eaa2c52a56c101bf5c1abb5cdc0a0
Kyd <info@kydkit.com>
2021-03-04 11:12:36 +0100

    test form

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,10 +1,18 @@
-import { usecontext } from 'react';
-import { carContext } from '../contexts/CarContext';
+import { Form } from 'react-bootstrap'
 
 const filter = () => {
   return (
     <div className="filter">
-      
+      <Form>
+        <Form.Group controlId="formBasicRange">
+          <Form.Label>Price</Form.Label>
+          <Form.Control type="range" />
+        </Form.Group>
+        <Form.Group controlId="formBasicRange">
+          <Form.Label>Miles</Form.Label>
+          <Form.Control type="range" />
+        </Form.Group>
+      </Form>
     </div>
   );
 }
