971c5e699c189a6f7703df7cb86ca883ed498745
Kyd <info@kydkit.com>
2021-03-04 10:48:32 +0100

    created filter.js

---
+++ src/components/filter.js
@@ -0,0 +1,12 @@
+import { usecontext } from 'react';
+import { carContext } from '../contexts/CarContext';
+
+const filter = () => {
+  return (
+    <div className="filter">
+      
+    </div>
+  );
+}
+
+export default filter;
