e7e7db20889e993f5f6a173ca26c40bde72eb112
Oskar Anderberg <otto.anderberg@gmail.com>
2021-03-04 12:16:47 +0100

    Create Search funktion and onChange funktion
    Create Search funktion in FilterContext and onChange funktion in SearchFields.js

--- src/contexts/FilterContext.js
+++ src/contexts/FilterContext.js
@@ -4,12 +4,26 @@ import { CarContext } from "../contexts/CarContext"
 export const FilterContext = createContext();
 
 const FilterContextProvider = (props) => {
-    const { cars, setCars } = useContext(CarContext)
-
-    console.log(cars)
+    const { cars, setCars } = useContext(CarContext);
+    const { tempCars, setTempCars } = useContext([]);
+    setTempCars(cars);
+    
+    const search = (searchString) => {
+        setCars(tempCars);
+        setCars(cars.filter((car) => {
+            return car.make.toLowerCase().search(searchString.toLowerCase()) !==-1 ||  car.model.toLowerCase().search(searchString.toLowerCase()) !==-1 ||  car.year.toString().toLowerCase().search(searchString.toLowerCase()) !==-1;
+        }))
+    }
+    //   const searchList =(e)=>{    
+    //     setNewCars(cars.filter((car)=>{
+    //      return car.make.toLowerCase().search(e.target.value.toLowerCase()) !==-1 ;
+    //     }) 
+    //     )                    
+    // }
 
     const values =
     {
+        search
     }
 
     return(
