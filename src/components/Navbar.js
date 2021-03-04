be6c7f38cca7b99a38268f56d4f0d50d22cbbc84
Mikaela <M.Norrelokke@hotmail.com>
2021-03-04 11:50:07 +0100

    changes to nav with responsive changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,55 +4,48 @@ import { useContext } from "react";
 import { useHistory } from "react-router-dom"
 import { NavLink } from "react-router-dom";
 
-function Navbar () {
-  const { totalProducts,totalOrder } = useContext(CarContext);
+function Navbar() {
+  const { totalProducts, totalOrder } = useContext(CarContext);
   const history = useHistory()
-  
-  return (
-<<<<<<< Updated upstream
-  <ReactBootstrap.Navbar collapseOnSelect expand="sm" bg="info" variant="info">
-=======
-  <ReactBootstrap.Navbar collapseOnSelect expand="sm" bg="info" variant="dark">
 
-    
-    <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
->>>>>>> Stashed changes
-    <ReactBootstrap.Navbar.Brand>
-      <NavLink to="/">
-        <img src="/assets/images/logo.png" alt="Logo"/>
-      </NavLink>
-    </ReactBootstrap.Navbar.Brand>
-    <ReactBootstrap.Dropdown>
-          <ReactBootstrap.Dropdown.Toggle variant="info" id="dropdown-basic" >
-            <div className="cartBox"> 
-              <p>{totalProducts}</p>
-              <img src="/assets/icons/cartw.svg" alt="Cart"/>
-            </div>
-          </ReactBootstrap.Dropdown.Toggle>
-          <ReactBootstrap.Dropdown.Menu>
-           <div className="totalPrice"><p> Total: {totalOrder} Kr</p></div>
-            <ReactBootstrap.NavDropdown.Divider />
-            <ReactBootstrap.NavDropdown.Item
-            onClick={() => history.push("/cartpage")}
-            >Checkout</ReactBootstrap.NavDropdown.Item>
-          </ReactBootstrap.Dropdown.Menu>
-        </ReactBootstrap.Dropdown>
-    <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
-      <ReactBootstrap.Nav className="ml-auto">
-        <ReactBootstrap.Nav.Link>            
-          <NavLink to="/">
-            Home
+  return (
+    <ReactBootstrap.Navbar collapseOnSelect expand="sm" bg="info" variant="dark">
+      <ReactBootstrap.Navbar.Brand>
+        <NavLink to="/">
+          <img src="/assets/images/logo.png" alt="Logo" />
+        </NavLink>
+      </ReactBootstrap.Navbar.Brand>
+      <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
+        <ReactBootstrap.Nav className="ml-auto">
+          <ReactBootstrap.Nav.Link>
+            <NavLink to="/">
+              Home
           </NavLink>
-        </ReactBootstrap.Nav.Link>
-        <ReactBootstrap.Nav.Link>
-          <NavLink to="/about">
-             About
-          </NavLink>  
-        </ReactBootstrap.Nav.Link>
-       
-      </ReactBootstrap.Nav>
-    </ReactBootstrap.Navbar.Collapse>
-  </ReactBootstrap.Navbar>
+          </ReactBootstrap.Nav.Link>
+          <ReactBootstrap.Nav.Link eventKey={2} href="/About">
+            <NavLink to="/about">
+              About
+          </NavLink>
+          </ReactBootstrap.Nav.Link>
+          <ReactBootstrap.Dropdown>
+            <ReactBootstrap.Dropdown.Toggle variant="info" id="dropdown-basic" >
+              <div className="cartBox">
+                <p>{totalProducts}</p>
+                <img src="/assets/icons/cartw.svg" alt="Cart" />
+              </div>
+            </ReactBootstrap.Dropdown.Toggle>
+            <ReactBootstrap.Dropdown.Menu>
+              <ReactBootstrap.NavDropdown.Item href="#">TOTAL:{totalOrder} Kr</ReactBootstrap.NavDropdown.Item>
+              <ReactBootstrap.NavDropdown.Divider />
+              <ReactBootstrap.NavDropdown.Item href="#"
+                onClick={() => history.push("/cartpage")}
+              >Checkout</ReactBootstrap.NavDropdown.Item>
+            </ReactBootstrap.Dropdown.Menu>
+          </ReactBootstrap.Dropdown>
+        </ReactBootstrap.Nav>
+      </ReactBootstrap.Navbar.Collapse>
+    </ReactBootstrap.Navbar>
   )
 }
 
