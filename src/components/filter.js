0f9c25320ea23cfdc124a02a2872d6b4e559d31d
Kyd <info@kydkit.com>
2021-03-04 18:02:54 +0100

    installed bootstrap range slider, added onChange functions

--- src/components/filter.js
+++ src/components/filter.js
@@ -2,18 +2,33 @@ import { useState, useContext } from 'react'
 import { FilterContext } from '../contexts/FilterContext'
 import styles from '../css/filter.module.css'
 import { Col, Row, Form } from 'react-bootstrap'
+import RangeSlider from 'react-bootstrap-range-slider';
 
 const Filter = () => {
   const [ priceValue1, setPriceValue1 ] = useState(0);
   const [ priceValue2, setPriceValue2 ] = useState(25);
-  
   const [ milesValue1, setMilesValue1 ] = useState(0);
   const [ milesValue2, setMilesValue2 ] = useState(25);
 
-  console.log(priceValue1);
-  console.log(priceValue2);
-  console.log(milesValue1);
-  console.log(milesValue2);
+  const handlePriceValue1 = (e) => {
+    console.log(priceValue1);
+    setPriceValue1(e.target.value)
+  }
+
+  const handlePriceValue2 = (e) => {
+    console.log(priceValue2);
+    setPriceValue2(e.target.value)
+  }
+
+  const handleMilesValue1 = (e) => {
+    console.log(milesValue1);
+    setMilesValue1(e.target.value)
+  }
+
+  const handleMilesValue2 = (e) => {
+    console.log(milesValue2);
+    setMilesValue2(e.target.value)
+  }
 
   return (
     <div>
@@ -26,14 +41,14 @@ const Filter = () => {
               <p>Price</p>
               <Row>
                 <p>min</p>
-                <Col xs="6">
-                  <input type="range" value={priceValue1} onChange={e => setPriceValue1(e.target.value)} min="0" max="800000" step="10000"/>
+                <Col xs="9">
+                  <RangeSlider value={priceValue1} onChange={handlePriceValue1} min="0" max="800000" step="10000"/>
                 </Col>
               </Row>
               <Row>
                 <p>max</p>
-                <Col xs="6">
-                  <input type="range" value={priceValue2} onChange={e => setPriceValue2(e.target.value)} min="0" max="800000" step="10000"/>
+                <Col xs="9">
+                  <RangeSlider value={priceValue2} onChange={handlePriceValue2} min="0" max="800000" step="10000"/>
                 </Col>
               </Row>
             </Form.Group>
@@ -42,14 +57,14 @@ const Filter = () => {
               <p>Miles</p>
               <Row>
                 <p>min</p>
-                <Col xs="6">
-                  <input type="range" value={milesValue1} onChange={e => setMilesValue1(e.target.value)} min="0" max="70000" step="1000"/>
+                <Col xs="9">
+                  <RangeSlider value={milesValue1} onChange={handleMilesValue1} min="0" max="70000" step="1000"/>
                 </Col>
               </Row>
               <Row>
                 <p>max</p>
-                <Col xs="6">
-                  <input type="range" value={milesValue2} onChange={e => setMilesValue2(e.target.value)} min="0" max="70000" step="1000"/>
+                <Col xs="9">
+                  <RangeSlider value={milesValue2} onChange={handleMilesValue2} min="0" max="70000" step="1000"/>
                 </Col>
               </Row>
             </Form.Group>
