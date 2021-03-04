1c28a67396d2054d067ba4f45f2ce568d442563a
Hitomi <hitomi.winberg@gmail.com>
2021-03-04 14:51:03 +0100

    Fixed function search

--- src/contexts/FilterContext.js
+++ src/contexts/FilterContext.js
@@ -1,40 +1,23 @@
 import {createContext, useContext, useState, useEffect} from "react";
 import { CarContext } from "../contexts/CarContext"
-
 export const FilterContext = createContext();
 
 const FilterContextProvider = (props) => {
     const { cars, setCars, tempCars } = useContext(CarContext);
 
-    const search = (searchString) => {   
-        
-        /*
-        setCars(cars.map((car) => {
-            if (!searchString) {
-                return car
-            } else if (car.make.toLowerCase().search(searchString.toLowerCase()) !==-1) {
-                return car
-            }
-        }))
-        */
-
-        
-        /*
-        if (!searchString) {
-            setCars(tempCars)
-        } else {
-            setCars(cars.filter((car) => {
-                return car.make.toLowerCase().search(searchString.toLowerCase()) !==-1 ||  car.model.toLowerCase().search(searchString.toLowerCase()) !==-1 ||  car.year.toString().toLowerCase().search(searchString.toLowerCase()) !==-1;
-            }))
-        } 
-        */
+    const search = (searchString) => {      
+        setCars(tempCars.filter((car) => {
+            console.log(searchString)              
+            return car.make.toLowerCase().search(searchString.toLowerCase()) !==-1 
+            // ||  car.model.toLowerCase().search(searchString.toLowerCase()) !==-1
+            // ||  car.year.toString().toLowerCase().search(searchString.toLowerCase()) !==-1;
+        }))              
     }
-
     const values =
     {
         search
     }
-
+    
     return(
         <FilterContext.Provider value={values}>
         {props.children}
