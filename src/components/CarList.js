fd0c428e0ffd9ceccd1c5f56d864604c40cb8d07
celiltat2361 <celilbtc@gmail.com>
2021-03-04 11:19:47 +0100

    Added sort.js components

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -2,10 +2,13 @@ import {useContext} from "react"
 import {CarContext }from "../contexts/CarContext"
 import Car from "./Car"
 import {Container, Row } from 'react-bootstrap'
+import Sort from "./Sort"
+
 function CarList() {
   const {cars} =useContext(CarContext)
   return (         
      <div className="carlist-container">
+      <Sort />
       <Container fluid>
         <Row className="d-flex justify-content-center align-content-center flex-wrap">
         {cars.map((car)=>(        
