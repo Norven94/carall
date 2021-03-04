a6bce508e7479cb3de542067457504e7a77b1495
Kyd <info@kydkit.com>
2021-03-04 15:34:50 +0100

    Filter with min and max range

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,22 +1,61 @@
 import { useState } from 'react'
 import styles from '../css/filter.module.css'
 
+import { Col, Row, Form } from 'react-bootstrap'
+
+
 const Filter = () => {
-  const [value, setValue] = useState([10,50])
+  //price useState
+  const [ priceValue1, setPriceValue1 ] = useState(0);
+  const [ priceValue2, setPriceValue2 ] = useState(25);
+  
+  //miles useState
+  const [ milesValue1, setMilesValue1 ] = useState(0);
+  const [ milesValue2, setMilesValue2 ] = useState(25);
 
   return (
+    <div>
       <div className={styles.dropdown}>
         <button className={styles.dropbtn}>Filter</button>
+
         <div className={styles.dropdownContent}>
-          <form action="">
-            <label for="miles">Miles</label>
-            <input type="range" name="miles" id="miles" min="0" max="100" />
-            <label for="price">Price</label>
-            <input type="range" name="price" min="0" max="100"/>
-            
-          </form>
+          <Form className={styles.priceFilter}>
+            <Form.Group >
+              <p>Price</p>
+              <Row>
+                <p>min</p>
+                <Col xs="6">
+                  <input type="range" value={priceValue1} onChange={e => setPriceValue1(e.target.value)} min={0} max={50}/>
+                </Col>
+              </Row>
+              <Row>
+                <p>max</p>
+                <Col xs="6">
+                  <input type="range" value={priceValue2} onChange={e => setPriceValue2(e.target.value)} min={0} max={50}/>
+                </Col>
+              </Row>
+            </Form.Group>
+
+            <Form.Group>
+              <p>Miles</p>
+              <Row>
+                <p>min</p>
+                <Col xs="6">
+                  <input type="range" value={milesValue1} onChange={e => setMilesValue1(e.target.value)} min={0} max={50}/>
+                </Col>
+              </Row>
+              <Row>
+                <p>max</p>
+                <Col xs="6">
+                  <input type="range" value={milesValue2} onChange={e => setMilesValue2(e.target.value)} min={0} max={50}/>
+                </Col>
+              </Row>
+            </Form.Group>
+          </Form>
         </div>
+
       </div>
+    </div>
   );
 }
 
