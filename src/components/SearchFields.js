4bd9c908e8fc5a8891a2d79739b2a37f61b22e13
Hitomi <hitomi.winberg@gmail.com>
2021-03-04 11:22:19 +0100

    Add useContext to SearchFields

--- src/components/SearchFields.js
+++ src/components/SearchFields.js
@@ -1,5 +1,14 @@
-
+import { useContext, useState } from "react"
+import {CarContext }from "../contexts/CarContext"
 function SearchFields() {
+  const cars = useContext(CarContext)
+
+//   const searchList =(e)=>{    
+//     setNewCars(cars.filter((car)=>{
+//      return car.make.toLowerCase().search(e.target.value.toLowerCase()) !==-1 ;
+//     }) 
+//     )                    
+// }
   return (
     <div className="search-container">
     <div>
