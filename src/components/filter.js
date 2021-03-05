f6e65543fc132b9261085654768bf29568fa110a
Kyd <info@kydkit.com>
2021-03-05 12:02:30 +0100

    function to price range

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,34 +1,39 @@
 import { useState, useContext } from 'react'
 import { FilterContext } from '../contexts/FilterContext'
+
 import styles from '../css/filter.module.css'
 import { Col, Row, Form } from 'react-bootstrap'
-import RangeSlider from 'react-bootstrap-range-slider';
+import RangeSlider from 'react-bootstrap-range-slider'
 
 const Filter = () => {
+  const { filterPriceStart, filterPriceEnd } = useContext(FilterContext);
   const [ priceValue1, setPriceValue1 ] = useState(0);
   const [ priceValue2, setPriceValue2 ] = useState(25);
   const [ milesValue1, setMilesValue1 ] = useState(0);
   const [ milesValue2, setMilesValue2 ] = useState(25);
 
   const handlePriceValue1 = (e) => {
-    console.log(priceValue1);
-    setPriceValue1(e.target.value)
+    // console.log(priceValue1);
+    setPriceValue1(e.target.value);
+    filterPriceStart(e.target.value)
   }
 
   const handlePriceValue2 = (e) => {
-    console.log(priceValue2);
-    setPriceValue2(e.target.value)
+    // console.log(priceValue2);
+    setPriceValue2(e.target.value);
+    filterPriceEnd(e.target.value)
   }
 
   const handleMilesValue1 = (e) => {
-    console.log(milesValue1);
+    // console.log(milesValue1);
     setMilesValue1(e.target.value)
   }
 
   const handleMilesValue2 = (e) => {
-    console.log(milesValue2);
+    // console.log(milesValue2);
     setMilesValue2(e.target.value)
   }
+
 
   return (
     <div>
--- src/contexts/FilterContext.js
+++ src/contexts/FilterContext.js
@@ -4,6 +4,8 @@ export const FilterContext = createContext();
 
 const FilterContextProvider = (props) => {
     const { cars, setCars, tempCars } = useContext(CarContext);
+    const [priceStart, setPriceStart] = useState(0)
+    const [priceEnd, setPriceEnd] = useState(0)
 
     const search = (searchString) => {      
         setCars(tempCars.filter((car) => {
@@ -13,9 +15,25 @@ const FilterContextProvider = (props) => {
             ||  car.year.toString().toLowerCase().search(searchString.toLowerCase()) !==-1;
         }))              
     }
+
+    const filterPriceStart = (price) => {
+        setPriceStart(price)
+    }
+    const filterPriceEnd = (price) => {
+        setPriceEnd(price)
+    }
+
+    useEffect(() => {
+        setCars(tempCars.filter((car) => {
+            return car.price < priceEnd && car.price > priceStart
+        }))
+    }, [priceStart, priceEnd])
+
     const values =
     {
-        search
+        search, 
+        filterPriceEnd,
+        filterPriceStart
     }
     
     return(
