4b58df87fa571989a8cdcdd41388eba4e98267d1
Hitomi <hitomi.winberg@gmail.com>
2021-03-05 15:20:06 +0100

    Add margin

--- src/components/SearchGroup.js
+++ src/components/SearchGroup.js
@@ -1,10 +1,10 @@
 import SearchFields from "./SearchFields";
 import FilterRange from './FilterRange'
 import {Container, Row } from 'react-bootstrap'
-
+import styles from '../css/SearchGroup.module.css'
 function SearchGroup() { 
   return (         
-    <div className="search-group-container">
+    <div className={styles.container}>
       <Container fluid>
         <Row className="d-flex justify-content-center align-content-center flex-wrap">
           <FilterRange />
