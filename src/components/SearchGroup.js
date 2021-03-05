b327a1e236468b1a2fecef0bf330d91d896adfb1
Hitomi <hitomi.winberg@gmail.com>
2021-03-05 14:35:16 +0100

    Add style SearchGroup

--- src/components/SearchGroup.js
+++ src/components/SearchGroup.js
@@ -1,11 +1,16 @@
 import SearchFields from "./SearchFields";
 import FilterRange from './FilterRange'
+import {Container, Row } from 'react-bootstrap'
 
 function SearchGroup() { 
   return (         
     <div className="search-group-container">
-      <FilterRange />
-      <SearchFields />
+      <Container fluid>
+        <Row className="d-flex justify-content-center align-content-center flex-wrap">
+          <FilterRange />
+          <SearchFields />
+        </Row>
+      </Container>
     </div>
   )
 }
