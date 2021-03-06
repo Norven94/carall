95150225df1ec8cecfd1e88197b2685fb389d172
celiltat2361 <celilbtc@gmail.com>
2021-03-06 13:26:30 +0100

    fixed code format, deleted comment line and added new codes

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,26 +1,24 @@
-import { useContext, useEffect, useState } from "react"
-import { CarContext }from "../contexts/CarContext"
-import { FilterContext }from "../contexts/FilterContext"
+import { useContext } from "react";
+import { FilterContext } from "../contexts/FilterContext";
 
 function Sort() {
-  const {sort} = useContext(FilterContext);
+  const { sort } = useContext(FilterContext);
 
   const handleChange = (e) => {
     sort(e.target.value);
   };
 
-  return (         
-     
-       <div className="sort-container">
-        <span>Sort By:</span>
-        <select onChange={handleChange}>
-          <option value="make">Mark a-z</option>
-          <option value="model">Model a-z</option>
-          <option value="year up">Year ↑</option>
-          <option value="year down">Year ↓</option>
-        </select>
-      </div>
-  )
+  return (
+    <div className="sort-container">
+      <span>Sort By:</span>
+      <select onChange={handleChange}>
+        <option value="make">Mark a-z</option>
+        <option value="model">Model a-z</option>
+        <option value="year up">Year ↑</option>
+        <option value="year down">Year ↓</option>
+      </select>
+    </div>
+  );
 }
 
-export default Sort
+export default Sort;
