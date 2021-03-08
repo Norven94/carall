668c05ed5984e0c454a9e49e9a5e732b1301da9c
Kyd <info@kydkit.com>
2021-03-08 23:35:44 +0100

    Sale Tag on discount cars

--- src/components/DiscountCarList.js
+++ src/components/DiscountCarList.js
@@ -6,17 +6,19 @@ import Carousel from 'react-elastic-carousel'
 import '../css/DiscountCarList.module.css'
 import { alignPropType } from "react-bootstrap/esm/DropdownMenu"
 
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 1},
+  { width: 768, itemsToShow: 2 },
+  { width: 1200, itemsToShow: 3 }
+]
+
 const DiscountCarList = () => {
   const {carDiscount} = useContext(CarContext)
-  const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 1},
-    { width: 768, itemsToShow: 2 },
-    { width: 1200, itemsToShow: 3 }
-  ];
 
   return (
-    <div className="cardiscount-container">
+    <div className="discount">
       <h2>Discount Cars</h2>
       <Carousel breakPoints={breakPoints}>
         {carDiscount.map((car) =>(
