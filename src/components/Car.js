7e9b34232abcfbcdccc82506c49e823722a4c6b0
celiltat2361 <celilbtc@gmail.com>
2021-03-08 19:22:15 +0100

    added new style codes

--- src/components/Car.js
+++ src/components/Car.js
@@ -26,9 +26,22 @@ export default function Car(props) {
     );
   } */
 
+  const Button1 = () => {
+    return (
+      <button className={cartBox} onClick={() => addToCart(props.car)}>
+      <img
+        src="/assets/icons/cartw.svg"
+        alt="Cart"
+        className={cartBox}
+        onClick={() => addToCart(props.car)}
+      />
+    </button>
+    )
+  }
+
   return (
     <div>
-      <div className={car}>
+      <div className={car} >
         <img
           onClick={goToProduct}
           src={props.car.image}
@@ -48,15 +61,10 @@ export default function Car(props) {
           </h3>
           <h4 style={{paddingTop: "30px"}} className={props.car.isDiscount ? isdiscount : undiscount}>
             {priceWithSpace} kr{" "}
-            <button className={cartBox} onClick={() => addToCart(props.car)}>
-              <img
-                src="/assets/icons/cartw.svg"
-                alt="Cart"
-                className={cartBox}
-                onClick={() => addToCart(props.car)}
-              />
-            </button>
+            <Button1 />
+            
           </h4>
+          
           <hr />
           {/*  <button onClick={() => addToCart(props.car)}>Add to Cart</button> */}
           <div style={{ paddingLeft: "0px" }}>{props.car.city}</div>
