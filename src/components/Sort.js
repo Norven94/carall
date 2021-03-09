21062eb2a2072da8a37194f65ca22f748cb068c6
Stephanie <puchastephanie@gmail.com>
2021-03-09 15:46:47 +0100

    Styling done
    Styling done, filter position, filter thumb to dark color, sortby to bootstrap

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { FilterContext } from "../contexts/FilterContext";
+import Dropdown from 'react-bootstrap/Dropdown'
 
 function Sort() {
   const { sort } = useContext(FilterContext);
@@ -9,9 +10,22 @@ function Sort() {
   };
 
   return (
-    <div className="sort-container">
-      <span>Sort By:</span>
+    <div className="sort-container col-md-2">
+      <span>Sort By :</span>
+      {/* <Dropdown>
+        <Dropdown.Toggle id="dropdown-basic">
+          Sort by:
+  </Dropdown.Toggle>
+
+        <Dropdown.Menu onChange={handleChange}>
+          <Dropdown.Item as="button" value="make">Mark a-z</Dropdown.Item>
+          <Dropdown.Item value="model">Model a-z</Dropdown.Item>
+          <Dropdown.Item value="year up">Year ↑</Dropdown.Item>
+          <Dropdown.Item value="year down">Year ↓</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown> */}
       <select onChange={handleChange}>
+      <option selected>Unsorted</option>
         <option value="make">Mark a-z</option>
         <option value="model">Model a-z</option>
         <option value="year up">Year ↑</option>
