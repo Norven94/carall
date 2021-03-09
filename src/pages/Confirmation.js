45e9d71d3a8a46b6aab4d55de4b744139bcf71fe
Kyd <info@kydkit.com>
2021-03-09 14:07:34 +0100

    confirmation html partial done

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -1,9 +1,34 @@
-import { orderDetails } from "../contexts/CartContext"
+import  { useContext, useState, useEffect } from 'react'
+import { CartContext } from "../contexts/CartContext"
+
+import styles from '../css/Confirmation.module.css'
 
 const Confirmation = () => {
-  return (
-    <div className="confirmation">
+  const { orderDetails, cart } = useContext(CartContext)
+  const [confirm, setConfirm] = useState([])
+  // console.log(cart);
+  // console.log(orderDetails.cart);
+  // console.log(orderDetails)
 
+  useEffect(
+    () => {
+      setConfirm(cart.map((c) => {
+        <p>{c.make}</p>
+      }))
+    },[cart]
+  )
+
+  return (
+    <div className={styles.confirmation}>
+      <h1>Thank you for your purchase!</h1>
+      <h3>Your order details</h3>
+      <p>Full Name: {orderDetails.ShippingName}</p>
+      <p>Delivery Address: {`${orderDetails.ShippingAddress}, ${orderDetails.ShippingCity}, ${orderDetails.ShippingCountry}`}</p>
+      <h3>Car details:</h3>
+      <p>Make: {confirm}</p>
+      
+      
+      <button>Print order confirmation</button>
     </div>
   );
 }
