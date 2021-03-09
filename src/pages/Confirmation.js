66a2aa5e3b3203f7457ebe7b022eff44d37520ad
Kyd <info@kydkit.com>
2021-03-09 11:49:59 +0100

    created Confirmation page and imported CartContext.js

---
+++ src/pages/Confirmation.js
@@ -0,0 +1,11 @@
+import { orderDetails } from "../contexts/CartContext"
+
+const Confirmation = () => {
+  return (
+    <div className="confirmation">
+
+    </div>
+  );
+}
+
+export default Confirmation;
