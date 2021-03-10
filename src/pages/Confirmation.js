55031252128898a52b4f4a543882cfb06f303599
Kyd <info@kydkit.com>
2021-03-10 16:21:06 +0100

    confirmation styling done, missing print icon and separation line

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { CartContext } from "../contexts/CartContext"
+import { confirm } from '../css/confirm.module.css'
 
 import styles from '../css/Confirmation.module.css'
 
@@ -8,41 +9,48 @@ const Confirmation = () => {
 
   return (
     <div className={styles.confirmation}>
+      <img className={confirm} src="/assets/icons/confirm.svg" alt="confirm icon"/>
       <div className={styles.thanks}>
         <h1>Thank you for choosing us!</h1>
         <p>Below you will find information about your purchase</p>
       </div>
+
       <div className={styles.details}>
-        <h4>Order details:</h4>
-        <div className={styles.orderDetails}>
-          <p className={styles.left}>Order Number:</p>
-          <p className={styles.left}>Order Date:</p>
-          <p className={styles.left}>Full Name:</p>
-          <p className={styles.left}>Delivery Address:</p>
-          <p className={styles.right}> {orderDetails.orderNumber}</p>
-          <p className={styles.right}> {orderDetails.orderDate}</p>
-          <p className={styles.right}> {orderDetails.ShippingName}</p>
-          <p className={styles.right}> {orderDetails.ShippingAddress}</p>
-          <p className={styles.right}> {`${orderDetails.ShippingCity}, ${orderDetails.ShippingCountry}`}</p>
+      <h4>Order details:</h4>
+      <div className={styles.orderDetails}>
+        <div className={styles.detailsLeft}>
+          <p>Order Number:</p>
+          <p>Order Date:</p>
+          <p>Full Name:</p>
+          <p>Delivery Address:</p>
+        </div>
+        <div className={styles.detailsRight}>
+          <p> {orderDetails.orderNumber}</p>
+          <p> {orderDetails.orderDate}</p>
+          <p> {orderDetails.ShippingName}</p>
+          <p> {orderDetails.ShippingAddress}</p>
+          <p> {`${orderDetails.ShippingCity}, ${orderDetails.ShippingCountry}`}</p>
+        </div>
         </div>
 
         <h4>Car details:</h4>
         <div className={styles.carDetails}>
-          <p className={styles.left}>Article Number</p>
-          <p className={styles.left}>Make, Model:</p>
-          <p className={styles.left}>Price:</p>
-          <p > {orderDetails.vin}</p>
-        </div>
-        {orderDetails.cart.map((c) => (
-          <div key={c.vin}>
-            <p className={styles.right}>{c.vin}</p>
-            <p className={styles.right}>{`${c.make} ${c.model}`}</p>
-            <p className={styles.right}>{c.price} Kr</p>
+          <div className={styles.detailsLeft}>
+            <p className={styles.left}>Article Number</p>
+            <p className={styles.left}>Make, Model:</p>
+            <p className={styles.left}>Price:</p>
+            <p > {orderDetails.vin}</p>
           </div>
-        ))}
+          {orderDetails.cart.map((c) => (
+            <div className={styles.detailsRight} key={c.vin}>
+              <p className={styles.right}>{c.vin}</p>
+              <p className={styles.right}>{`${c.make} ${c.model}`}</p>
+              <p className={styles.right}>{c.price} Kr</p>
+            </div>
+          ))}
+        </div>
       </div>
-
-      <button onClick={() => window.print()}>Print </button>
+      <button className={styles.print} onClick={() => window.print()}>Print </button>
     </div>
   );
 }
