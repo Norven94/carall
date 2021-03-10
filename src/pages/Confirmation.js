7264113246ff7e5a74b58f3052ea048c0bd90522
Kyd <info@kydkit.com>
2021-03-10 15:32:42 +0100

    Roboto Condensed to index.css, updating confirmation page

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -8,20 +8,41 @@ const Confirmation = () => {
 
   return (
     <div className={styles.confirmation}>
-      <h1 className={styles.thanks}>Thank you for choosing us!</h1>
-      <p>Below you will find information about your purchase</p>
-      <h4>Order details:</h4>
-      <p>Full Name: {orderDetails.ShippingName}</p>
-      <p>Delivery Address: {`${orderDetails.ShippingAddress}, ${orderDetails.ShippingCity}, ${orderDetails.ShippingCountry}`}</p>
-      <h4>Car details:</h4>
-      {orderDetails.cart.map((c) => (
-        <div key={c.vin}>
-          <p>Article Number: {c.vin}</p>
-          <p>Make: {c.make}</p>
-          <p>Model: {c.model}</p>
+      <div className={styles.thanks}>
+        <h1>Thank you for choosing us!</h1>
+        <p>Below you will find information about your purchase</p>
+      </div>
+      <div className={styles.details}>
+        <h4>Order details:</h4>
+        <div className={styles.orderDetails}>
+          <p className={styles.left}>Order Number:</p>
+          <p className={styles.left}>Order Date:</p>
+          <p className={styles.left}>Full Name:</p>
+          <p className={styles.left}>Delivery Address:</p>
+          <p className={styles.right}> {orderDetails.orderNumber}</p>
+          <p className={styles.right}> {orderDetails.orderDate}</p>
+          <p className={styles.right}> {orderDetails.ShippingName}</p>
+          <p className={styles.right}> {orderDetails.ShippingAddress}</p>
+          <p className={styles.right}> {`${orderDetails.ShippingCity}, ${orderDetails.ShippingCountry}`}</p>
         </div>
-      ))}
-      <button onClick={() => window.print()}>Print order confirmation</button>
+
+        <h4>Car details:</h4>
+        <div className={styles.carDetails}>
+          <p className={styles.left}>Article Number</p>
+          <p className={styles.left}>Make, Model:</p>
+          <p className={styles.left}>Price:</p>
+          <p > {orderDetails.vin}</p>
+        </div>
+        {orderDetails.cart.map((c) => (
+          <div key={c.vin}>
+            <p className={styles.right}>{c.vin}</p>
+            <p className={styles.right}>{`${c.make} ${c.model}`}</p>
+            <p className={styles.right}>{c.price} Kr</p>
+          </div>
+        ))}
+      </div>
+
+      <button onClick={() => window.print()}>Print </button>
     </div>
   );
 }
