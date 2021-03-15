30df04ea16f901f442a7b6fc38f63f5710f6608a
Kyd <info@kydkit.com>
2021-03-15 14:26:42 +0100

    registration form and onChange handlers created

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,11 @@
+// import { Users } from '../contexts/UserContext'
+
 const Register = () => {
   return (
     <div className="register">
-      
+      <input onChange={handleRegisterName} placeholder="Name" value="" required/>
+      <input onChange={handleRegisterEmail} placeholder="Email" value="" required/>
+      <input onChange={handleRegisterPassword} placeholder="Password" value="" required/>
     </div>
   );
 }
