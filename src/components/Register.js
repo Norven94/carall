424258c36e6feb868e862e83372c1cb4efb96f0e
Kyd <info@kydkit.com>
2021-03-16 00:12:18 +0100

    created users array and onSubmit function for registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,21 @@
-// import { Users } from '../contexts/UserContext'
+import { useState, useContext } from 'react'
+import { UserContext } from '../contexts/UserContext'
 
 const Register = () => {
+  const { addToRegistration } = useContext(UserContext)
+  const [userName, setUserName] = useState();
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+
   return (
-    <div className="register">
-      <input onChange={handleRegisterName} placeholder="Name" value="" required/>
-      <input onChange={handleRegisterEmail} placeholder="Email" value="" required/>
-      <input onChange={handleRegisterPassword} placeholder="Password" value="" required/>
-    </div>
+    
+      <form onSubmit={(e) => addToRegistration(e, userName, password, email)}>
+        <input onChange={(e) => setUserName(e.target.value)}  placeholder="Name" required/>
+        <input onChange={(e) => setEmail(e.target.value)}  placeholder="Email" required/>
+        <input onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
+        <button type="submit">Register</button>
+      </form>
+    
   );
 }
 
