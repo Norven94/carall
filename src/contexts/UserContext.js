7ea31a9d8efd501f28d2eac77e74fc89d76f2083
Hitomi <hitomi.winberg@gmail.com>
2021-03-17 10:58:50 +0100

    Stop to add same member

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,28 +1,34 @@
 import {createContext, useState, useEffect} from "react"
-
 export const UserContext = createContext();
 
 const UserContextProvider = (props) => {  
     const [loginState, setLoginState] = useState(false);
-    const [isMember, setIsMember] = useState(true);
+    const [isMember, setIsMember] = useState(false);
     const [users, setUsers] = useState([]);
-
     const addToRegistration = (e, email, password) => {
         e.preventDefault()
         const member = {
             email, 
             password
-        }
-        setUsers([member, ...users])
-        
-        console.log(member.email);
-        console.log(users)
-        users.map((user) => {
-            if(user.email === member.email) {
-                return setIsMember(false)
+        } 
+        let isAlreadyMember =false   
+        for(let i =0; i< users.length; i++){
+            if(users[i].email === member.email) {
+                  setIsMember(true);
+                isAlreadyMember=true
+                console.log('already_a_member')               
             }
-        })
+        }   
+   
+        if (!isAlreadyMember) {
+            console.log('new member')
+            setUsers([member, ...users])
+        } 
+        else{
+            setIsMember(true)
+        }
     }
+    console.log(users)
 
     const values =
     {
