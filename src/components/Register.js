62cbbd7fd2e0faf79e1412ad84c2865f93abda95
Oskar Anderberg <otto.anderberg@gmail.com>
2021-03-18 11:21:11 +0100

    hotfix after testing

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,14 +2,23 @@ import { useState, useContext } from 'react'
 import { UserContext } from '../contexts/UserContext'
 
 const Register = () => {
-  const { addToRegistration, isMember } = useContext(UserContext)
+  const { addToRegistration, isMember, setIsMember } = useContext(UserContext)
   const [password, setPassword] = useState();
   const [email, setEmail] = useState();
+
+  const emailChange = (e) => {
+    setEmail(e.target.value)
+    setIsMember(false)
+  } 
+
+  const passwordChange = (e) => {
+    setPassword(e.target.value)
+  } 
+
   return (
       <form onSubmit={(e) => addToRegistration(e, email, password)}>
-        <input onChange={(e) => setEmail(e.target.value)}  placeholder="Email" required/>
-        {/* <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/> */}
-        <input onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
+        <input onChange={emailChange}  placeholder="Email" required/>
+        <input type="password" onChange={passwordChange} placeholder="Password" required/>
         <button type="submit">Register</button>
         <p >{isMember && "this email already exist"}</p>
       </form>
