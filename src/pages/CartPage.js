b82d4367051466c11982c44148a80ace10d01528
Jakob <jsandberg_8@hotmail.com>
2021-03-18 11:55:54 +0100

    CSS cartpage

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -11,7 +11,7 @@ import { Col, Container, Row, Form } from "react-bootstrap";
 import Back from '../components/Back'
 
 const CartPage = () => {
-  const { cart, setCart, orderDetails, setOrderDetails, billingDetails, shippingDetails, previousOrderDetails, setPreviousOrderDetails} = useContext(CartContext);
+  const { cart, setCart, orderDetails, setOrderDetails, billingDetails, shippingDetails, previousOrderDetails, setPreviousOrderDetails } = useContext(CartContext);
   const { users, setUsers, currentUser } = useContext(UserContext);
   const { cars, setCars } = useContext(CarContext);
   const history = useHistory();
@@ -20,54 +20,55 @@ const CartPage = () => {
     history.push("/confirmation")
     let timestamp = new Date().toLocaleDateString();
     let id = Math.floor(Math.random() * 100000);
-    setOrderDetails({billingDetails, shippingDetails, orderDate: timestamp, orderNumber: id, cart});    
-    
+    setOrderDetails({ billingDetails, shippingDetails, orderDate: timestamp, orderNumber: id, cart });
+
     //Reset car list and empty the cart after purchase 
     setCart([]);
     setCars(cars.map((car) => {
       car.purchased = false;
       return car
     }));
-  } 
+  }
 
   useEffect(() => {
-      setUsers(users.map((user) => {
-        if (user.email === currentUser.email) {          
-          setPreviousOrderDetails([...previousOrderDetails, orderDetails])
-          return {
-            ...user,
-            previousOrders: previousOrderDetails
-          } 
-        } else {
-          return user
+    setUsers(users.map((user) => {
+      if (user.email === currentUser.email) {
+        setPreviousOrderDetails([...previousOrderDetails, orderDetails])
+        return {
+          ...user,
+          previousOrders: previousOrderDetails
         }
-      }))
-  },[orderDetails])
+      } else {
+        return user
+      }
+    }))
+  }, [orderDetails])
 
   return (
     <div className={styles["cartPage-style"]}>
       <div className={styles["back-button"]}>
-      <Back/>
+        <Back />
       </div>
       <div className={styles["div"]}>
-        <h1 className={styles["cart-rubrik"]}>SHOPPING CART</h1>
         <div className={styles["product-container"]}>
-          {cart.map(product => <CartProduct key={product.vin} product={product} />)}
+          <h1 className={styles["cart-rubrik"]}>SHOPPING CART</h1>
+          <div className={styles["product"]}>
+            {cart.map(product => <CartProduct key={product.vin} product={product} />)}
+          </div>
         </div>
       </div>
-        <div className={styles["forms"]}>
-          <Form onSubmit={handleClick}>
-            <div className={styles["billing"]}>
-              <BillingFields />
-      
-            </div>
-            <div className={styles["shipping"]}>
-              <ShippingFields />
-              <button type="submit" className={styles.buyButton} >BUY</button>
-            </div>
-          </Form>
-        </div>
+      <div className={styles["forms"]}>
+        <Form onSubmit={handleClick}>
+          <div className={styles["billing"]}>
+            <BillingFields />
+          </div>
+          <div className={styles["shipping"]}>
+            <ShippingFields />
+            <button type="submit" className={styles.buyButton} >BUY</button>
+          </div>
+        </Form>
       </div>
+    </div>
   );
 }
 
