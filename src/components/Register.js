cf983d26ff7e97e289c9bfcc014bb1df068d3982
Hitomi <hitomi.winberg@gmail.com>
2021-03-19 16:43:16 +0100

    Style confirm password validation

--- src/components/Register.js
+++ src/components/Register.js
@@ -7,24 +7,48 @@ const Register = () => {
   const { addToRegistration, isMember, setIsMember } = useContext(UserContext)
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [confirmPassword, setConfirmPassword]=useState("")
+  const [isValid, setIsValid]=useState(false)
+  
   useEffect(() => {
    setIsMember(false)
   }, [])
 
+  useEffect(()=>{
+    if(password!="" && password===confirmPassword){
+      setIsValid(true)
+    }
+    else{
+      setIsValid(false)
+    }
+
+  },[password,confirmPassword ])
+
   const emailChange = (e) => {
     setEmail(e.target.value)
-    setIsMember(false)
   } 
-
 
   const passwordChange = (e) => {
-    setPassword(e.target.value)
+    setPassword(e.target.value) 
   } 
 
+  const checkPassword=(e)=>{ 
+    setConfirmPassword(e.target.value)  
+  }
+
+  const handleSubmit=(e)=>{
+    if(isValid){
+    addToRegistration(e, email, password)
+    }
+    else{
+      e.preventDefault();
+    }
+  }
+ 
   return (
     <div className="container col-md-6 py-5">
        <h1 className="text-center">Become a Member</h1>
-      <Form onSubmit={(e) => addToRegistration(e, email, password)}>
+      <Form onSubmit={handleSubmit}>
        <Alert variant={"danger"} className={`${styles.errorBox} ${isMember ? styles.active : styles.inactive}`}>This email already exist.</Alert>
        <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
@@ -32,11 +56,16 @@ const Register = () => {
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Create a password</Form.Label>
-          <Form.Control onChange={passwordChange} type="password" placeholder="Password" required/>
+          <Form.Control onChange={passwordChange} type="password" name="password" placeholder="Password" required/>
+        </Form.Group>
+        <Form.Group controlId="formConfirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control className={isValid ? "is-valid" : "is-invalid"} onChange={checkPassword} type="password" name="confirm" placeholder="Confirm Password" required/>
+        <Form.Control.Feedback className={`${styles.errorBox} ${isValid ? styles.active : styles.inactive}`}>OK! Click Submit</ Form.Control.Feedback>
         </Form.Group>
         <Container className="text-center">
           <Button className={styles.singInButton} variant="primary" type="submit">
-             REGISTER
+             Submit
           </Button>
         </Container>
       </Form>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,6 @@ function Home() {
     </div>      
   )
 }
-
 export default Home
 
 
