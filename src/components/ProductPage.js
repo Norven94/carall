c042b8bea76cbbacaf8d67026fdcdea10877e769
Stephanie <puchastephanie@gmail.com>
2021-03-23 16:00:16 +0100

    Changed to old version productpage & css
    Changed to old version productpage & css

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -6,7 +6,7 @@ import Carousel from 'react-elastic-carousel'
 import styles from "../css/productpage.module.css";
 import Back from './Back'
 import Car from './Car'
-import Footer from "../components/Footer"
+import Footer from "./Footer"
 // import Carousel from 'react-bootstrap/Carousel'
 
 export default function ProductPage(props) {
@@ -48,14 +48,19 @@ export default function ProductPage(props) {
 
   return (
     <>
+    <div>
+<p>Lorem <span className={styles.pChange}></span> </p>      
+    </div>
       <Back />
 
       <Container className={styles["product-page"]}>
-        <Row className={styles.rowContainer}>
-          <Col xs={11} sm={8} lg={5} className={styles["image-container"]}>
+        {/* <Row> */}
+          {/* <Col className={styles.colContainer}> */}
+          <Row className={styles.rowContainer}>
+          <Col xs={11} sm={8} lg={5} className={styles["image-container"]} >
             <span className={`${styles.discountTag} ${product.isDiscount ? styles.isdiscount : styles.undiscount}`}>Sale</span>
             <span className={`${styles.purchasedBox} ${product.purchased ? styles.purchased : styles.notPurchased}`}>In your cart</span>
-            <img
+            <img 
               src={product.image}
               alt={
                 "Image of " +
@@ -66,26 +71,28 @@ export default function ProductPage(props) {
                 product.year
               }
             />
-          </Col>
+            </Col>
           <Col xs={11} sm={8} lg={6} className={styles["product-details"]}>
-            <h1 className={styles["product-info"]}>{product.make}</h1>
+            <h1 className={styles["product-info"]}>Make  : {product.make}</h1>
             <div className={styles["details1"]}>
-              <div className={styles["product-makemiles"]}>
-                <h4>{product.model} / {product.year} / {product.miles} miles </h4>
-                <h6 className={styles["product-city"]}>{product.city}</h6>
+              <div className={styles.productmakemiles}>
+                <h4>Model : {product.model} </h4>
+                <h4>Year : {product.year} </h4>
+                <h4>Mileage : {product.miles} miles </h4>
+                <h4 className={styles["product-city"]}>Location : {product.city} </h4>
               </div>
               {/* <div className={styles["product-city-year"]}>
                   <h4>{product.miles} miles</h4>
                   <h6 className={styles["product-year"]}>{product.year}</h6>
                 </div> */}
             </div>
-            <ReadMore maxChar="100">{product.descLong}</ReadMore>
+            <ReadMore maxChar="100">{`Description: ${product.descLong}`}</ReadMore>
 
-            <span className={styles["product-price"]}>{priceWithSpace}Kr</span>
+            <span className={styles["product-price"]}>Price : {priceWithSpace} Kr </span>
 
             <button className={styles.cartBox1} onClick={() => addToCart(product)}>
               <img
-                src="../assets/icons/cart.svg"
+                src="../assets/icons/cart-orange.svg"
                 alt="Cart"
                 className={styles.cartBox1}
                 onClick={() => addToCart(product)}
@@ -93,7 +100,7 @@ export default function ProductPage(props) {
             </button>
           </Col>
         </Row>
-        <Col>
+        {/* <Col>
             <div className={styles.top1}>
             <img className={styles.check} src="/assets/icons/confirm.svg" alt="confirm icon" />
             <h4>Driving test service</h4>
@@ -106,24 +113,33 @@ export default function ProductPage(props) {
             <img className={styles.check} src="/assets/icons/confirm.svg" alt="confirm icon" />
             <h4>Get more discount as member</h4>
             </div>
-            </Col>
+            </Col> */}
       </Container>
 
       <div>
-        <h1 className={styles.h1Carousel}>Another {product.make} models</h1>
+        <h1 className={styles.h1Carousel}>You may also like this</h1>
       </div>
 
       <Carousel className={styles.carousel}>
-        <Carousel.Item className={styles.carousel}>
+      {/* <CarouselItem className={styles.carousel}> */}
           {cars.map((car) => {
-            if (product.make === car.make && product.vin !== car.vin) {
+            if (product.make === car.make || product.year === car.year && product.vin !== car.vin ) {
               return (
                 <Car key={car.vin} car={car} />
               )
             }
           })}
-        </Carousel.Item>
+         {/* </CarouselItem> */}
       </Carousel>
+
+      <h4 className={styles.formH4}>Subscribe our newsletter and get the best deals for your car.</h4>
+      <Form className={styles.subscribe}>
+        <input type="text" className={styles.inputEmail} name="emailaddress" placeholder="Your email address here..."></input><button>SEND</button>
+      </Form>
+      <div>
+      <Footer />
+      </div>
+      
 
     </>
   );
