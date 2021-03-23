6f43c829871e54af40ceba3c80d64a816ad20beb
Stephanie <puchastephanie@gmail.com>
2021-03-23 15:47:42 +0100

    Change to old version of productpage css
    Change to old version of productpage css

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -1,11 +1,18 @@
 import { UserContext } from "../contexts/UserContext";
 import { useContext, useEffect } from "react";
+import styles from "../css/WelcomeText.module.css"
+
 export default function WelcomeText () {
   const { loginState } = useContext(UserContext);
 
   return (
     <div className="d-flex flex-row-reverse">
        <p className="text-center col-12" style={{ margin:"0", color:"white", backgroundColor:"#FE7F50" }}>{loginState && "You are logged in"}</p>
-    </div>     
+       {/* <p className={styles.pContact}>{loginState && "Test"}</p> */}
+       {/* <div className={styles.pContact}>{loginState && "Test"}</div> */}
+       {/* <p className={styles.pContact}></p>  */}
+       {/* <p className={styles.pContact}>{loginState && "Contact us 24/7"}</p> */}
+     
+    </div>   
   )
 }
