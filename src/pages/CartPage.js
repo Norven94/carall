5732daebef8f8b2152bfbbd6cacc1e368f19a42c
Kyd <info@kydkit.com>
2021-03-24 09:46:03 +0100

    Cart page clean up

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -13,10 +13,8 @@ import Back from '../components/Back'
 import Footer from '../components/Footer'
 import footerstyle from '../css/Footer.module.css'
 
-
-
 const CartPage = () => {
-  const { cart, setCart, setOrderDetails, billingDetails, shippingDetails, errorLogin, setErrorLogin, formWarning, setFormWarning} = useContext(CartContext);
+  const { cart, setCart, setOrderDetails, billingDetails, shippingDetails, errorLogin, setErrorLogin, formWarning } = useContext(CartContext);
   const { cars, setCars, setTempCars } = useContext(CarContext);
   const { loginState } = useContext(UserContext);
   const history = useHistory();
@@ -25,27 +23,26 @@ const CartPage = () => {
     e.preventDefault()
     // checks if billingDetails have proper characters in its fields
     if (!formWarning) {
-        history.push("/confirmation")
+      history.push("/confirmation")
       if (loginState) {
         let timestamp = new Date().toLocaleDateString();
         let id = Math.floor(Math.random() * 100000);
-        setOrderDetails({billingDetails, shippingDetails, orderDate: timestamp, orderNumber: id, cart});    
+        setOrderDetails({ billingDetails, shippingDetails, orderDate: timestamp, orderNumber: id, cart });
         //Reset filtering tempcar after purchase
         setTempCars(cars)
         //Reset car list and empty the cart after purchase 
         setCart([]);
-      setCars(cars.map((car) => {
-        if (car.purchased) {
-          car.sold = true;
-        }
-        car.purchased = false;
-        return car
-      }));
-    } else {
-      setErrorLogin(true);
+        setCars(cars.map((car) => {
+          if (car.purchased) {
+            car.sold = true;
+          }
+          car.purchased = false;
+          return car
+        }));
+      } else {
+        setErrorLogin(true);
+      }
     }
-  } 
-
   }
 
   useEffect(() => {
@@ -75,41 +72,40 @@ const CartPage = () => {
   }
   else {
     return (
-        <div>
-      <div className={styles["cartPage-style"]}>
-        <div className={styles["back-button"]}>
-          <Back />
-        </div>
-        <div className={styles["div"]}>
-          <div className={styles["product-container"]}>
-            <h1 className={styles["cart-rubrik"]}>SHOPPING CART</h1>
-            <div className={styles["product"]}>
-              {cart.map(product => <CartProduct key={product.vin} product={product} />)}
+      <div>
+        <div className={styles["cartPage-style"]}>
+          <div className={styles["back-button"]}>
+            <Back />
+          </div>
+          <div className={styles["div"]}>
+            <div className={styles["product-container"]}>
+              <h1 className={styles["cart-rubrik"]}>SHOPPING CART</h1>
+              <div className={styles["product"]}>
+                {cart.map(product => <CartProduct key={product.vin} product={product} />)}
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles["forms"]}>
-          <Form onSubmit={handleClick}>
-            <div className={styles["billing"]}>
-              <BillingFields />
-            </div>
-            <div className={styles["shipping"]}>
-              <ShippingFields />
-              <Container>
-                <button type="submit" className={styles.buyButton} >BUY</button>
-              </Container>
-            </div>
-          </Form>
-        </div>
-        {errorLogin ? <PreventPurchase /> : ""}
-        <div>
-        </div>
-      </div>
-      <div className={footerstyle.sticky}>
-            <Footer />
+          <div className={styles["forms"]}>
+            <Form onSubmit={handleClick}>
+              <div className={styles["billing"]}>
+                <BillingFields />
+              </div>
+              <div className={styles["shipping"]}>
+                <ShippingFields />
+                <Container>
+                  <button type="submit" className={styles.buyButton} >BUY</button>
+                </Container>
+              </div>
+            </Form>
           </div>
+          {errorLogin ? <PreventPurchase /> : ""}
+          <div>
+          </div>
+        </div>
+        <div className={footerstyle.sticky}>
+          <Footer />
+        </div>
       </div>
-
     );
   }
 }
